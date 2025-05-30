5b0fef120ceefb8601ca5bbf3ce84cfb1ff5bf04
Gehu <lauric_gehu@etu.u-bourgogne.fr>
2025-05-30 11:28:04 +0200

    implementing command interpreter to ecg_sensor

--- src/ecg_sensor.adb
+++ src/ecg_sensor.adb
@@ -1,140 +1,142 @@
 with Ada.Exceptions; use Ada.Exceptions;
-with Ada.Real_Time;
+with Ada.Real_Time; use Ada.Real_Time;
 with Ada.Strings.Bounded;
 with Interfaces; use Interfaces;
 
 with HAL; use HAL;
 with HAL.UART; use HAL.UART;
 
-with UART_USB; use UART_USB;
+with UART_USB;
 with PanTompkins;
 with AdaData;
+with Commands_Interpreter;
 
 procedure Ecg_Sensor is
 
    ECG_VERSION : constant String := "0.1";
+   LF_CR : constant String := ASCII.LF & ASCII.CR;
 
    type Sampling_Mode is (Mode_Loop, Mode_Onetime);
-   type Sensor_State is (INIT, SAMPLING, PAUSED);
-   type Commands is (START, STOP, PAUSE, RESUME, VERSION, GET_PARAM, STATE);
-   type Parameters_Type is (SAMPLE_MODE, SAMPLE_RATE, OUTPUT_STAGE, AMPLITUDE_COEF, PICK_DISTANCE, WINDOW_SEC);
+   type Sensor_State_Type is (INIT, SAMPLING, PAUSED);
 
-   type Parameters_Array is array (Natural range <>) of IEEE_Float_32;
+   procedure Log (Msg : String) renames UART_USB.Transmit_String;
 
-   Parameters : Parameters_Array(0 .. Parameters_Type'Pos (Parameters_Type'Last));  
-   
-   CMD_END : constant String := ASCII.LF & ASCII.CR;
-
-   procedure Parse_Cmd(Command : UART_String; Arg : out UART_String; Value : out UART_String) is
-      Equal_Pos : Natural := B_Str.Index (Command, "=", 1);
+   procedure Return_Arg (Arg : Commands_Interpreter.Argument; Valid : Boolean) is
    begin
-      if B_Str.Length (Command) < 3 or Equal_Pos < 2 then
-         Arg := Command;
-         Value := B_Str.To_Bounded_String("None");
-         return;
-      end if; 
-      
-      Arg := B_Str.Bounded_Slice(Command, 1, Equal_Pos - 1);
-      Value := B_Str.Bounded_Slice(Command, Equal_Pos + 1, B_Str.Length (Command));
-
-   end Parse_Cmd;
-
-   function Get_Value(Param : Parameters_Type) return IEEE_Float_32 is
-   begin
-      return Parameters (Parameters_Type'Pos (Param));
-   end Get_Value;
-
-   procedure Set_Value(Param : Parameters_Type; Value : IEEE_Float_32) is 
-   begin
-      Parameters (Parameters_Type'Pos (Param)) := Value;
+      if Valid then
+         Log (Arg.Key'Image & " = " & Arg.Value'Image & LF_CR);
+      else
+         Log ("Invalid parameter" & LF_CR);
+      end if;
    end;
 
-   procedure Set_Default_Parameters is
+   procedure Change_State (Arg : Commands_Interpreter.Argument; Valid : Boolean) is
    begin
-      Set_Value (SAMPLE_RATE, 100.0);
-      Set_Value (OUTPUT_STAGE, IEEE_Float_32 (PanTompkins.Stage'Pos (PanTompkins.Stage_Integrated)));
-      Set_Value (AMPLITUDE_COEF, 1.5);
-      Set_Value (PICK_DISTANCE, 0.3);
-      Set_Value (WINDOW_SEC, 0.4);
-      Set_Value (SAMPLE_MODE, IEEE_Float_32 (Sampling_Mode'Pos (Mode_Loop)));
+      if Valid then
+         null;
+      end if;
    end;
 
-   procedure Print_Param is
+   procedure Print_Args (Input : Commands_Interpreter.Argument; Valid : Boolean) is
+   Args : Commands_Interpreter.Arg_Array (1 .. Commands_Interpreter.Get_Arg_Count);
    begin
-      for I in Parameters_Type'Range loop
-         UART_USB.Transmit_String (I'Image & "=" & Get_Value (I)'Image & CMD_END);
+      Commands_Interpreter.Get_Args (Args);
+      for I in Args'Range loop
+         Log (Args (I).Key'Image & " = " & Args (I).Value'Image & LF_CR);
       end loop;
-   end;
+   end Print_Args;
 
-   function Read_Param return PanTompkins.Config is
-      Command : UART_String;
-      Arg     : UART_String;
-      Value   : UART_String;
-      Conf : PanTompkins.Config;
-      Param : Parameters_Type;
-   begin
+   package Get_Args is new Commands_Interpreter.Action_Accessor (
+               Key => "GET_ARGS", 
+               Action_Fn => Print_Args'Access);
 
-      loop
-         Command := UART_USB.Receive_String (ASCII.Semicolon, Ada.Real_Time.Time_Span_Last);
-         Parse_Cmd (Command, Arg, Value);
+   package Sample_Rate is new Commands_Interpreter.Real_Accessor (T => PanTompkins.Sampling_Frequency_Type,
+               Key            => "SAMPLE_RATE",
+               Default_Value  => 100.0,
+               Action_Fn      => Return_Arg'Access
+            );
 
-         exit when B_Str.To_String (Arg) = "START";
+   package Sensor_State is new Commands_Interpreter.Discrete_Accessor (T => Sensor_State_Type,
+               Key            => "SENSOR_STATE",
+               Default_Value  => INIT,
+               Action_Fn      => Change_State'Access
+            );
 
-         -- Action
-         if B_Str.To_String(Value) = "None" then
-            case Commands'Value (B_Str.To_String (Arg)) is
-               when VERSION => UART_USB.Transmit_String ("ECG_SENSOR v" & ECG_VERSION & CMD_END);
-               when GET_PARAM => Print_Param;
-               when others => UART_USB.Transmit_String ("Unknown command" & CMD_END);
-            end case;
-         
-         -- Parameter
-         else 
-            begin
-               Param := Parameters_Type'Value (B_Str.To_String (Arg));
-               Parameters (Parameters_Type'Pos (Param)) := IEEE_Float_32'Value (B_Str.To_String (Value));
-               UART_USB.Transmit_String ("Ok" & CMD_END);
-            exception
-               when C : Constraint_Error =>
-                  UART_USB.Transmit_String (Arg'Image & " is not a valid parameter: " & Exception_Message (C) & CMD_END);
-            end;
-         end if;
-      end loop;
-      
-      return (Sampling_Frequency => Get_Value (SAMPLE_RATE), 
-               Amplitude_Treshold_Coef => Get_Value (AMPLITUDE_COEF), 
-               Minimal_Pick_Distance_Sec => Get_Value (PICK_DISTANCE), 
-               Window_Sec => Get_Value (WINDOW_SEC), 
-               Output_Stage => PanTompkins.Stage'Val (Natural (Get_Value (OUTPUT_STAGE))));
+   package Amplitude_Coef is new Commands_Interpreter.Real_Accessor (T => PanTompkins.Amplitude_Treshold_Coef_Type,
+               Key            => "AMPLITUDE_COEF",
+               Default_Value  => 1.5,
+               Action_Fn      => Return_Arg'Access
+            );
 
-   end Read_Param;
+   package Pick_Distance is new Commands_Interpreter.Real_Accessor (T => PanTompkins.Positive_Float,
+               Key            => "PICK_DISTANCE",
+               Default_Value  => 0.3,
+               Action_Fn      => Return_Arg'Access
+            );
+
+   package Window_Sec is new Commands_Interpreter.Real_Accessor (T => PanTompkins.Positive_Float,
+               Key            => "WINDOW_SEC",
+               Default_Value  => 0.4,
+               Action_Fn      => Return_Arg'Access
+            );
+
+   package Output_Stage is new Commands_Interpreter.Discrete_Accessor (T => PanTompkins.Stage,
+               Key            => "OUTPUT_STAGE",
+               Default_Value  => PanTompkins.Stage_Integrated,
+               Action_Fn      => Return_Arg'Access
+            );
+
+   procedure Initialize is begin
+      UART_USB.Initialize (115_200);
+      UART_USB.Transmit_String ("ECG_SENSOR v0.1" & LF_CR);
+
+      Amplitude_Coef.Register;
+      Sample_Rate.Register;
+      Pick_Distance.Register;
+      Window_Sec.Register;
+      Output_Stage.Register;
+      Sensor_State.Register;
+      Get_Args.Register;
+
+   end Initialize;
 
    Result : IEEE_Float_32;
-   Mode   : Sampling_Mode;
    Status : UART_Status;
-   Config : PanTompkins.Config;
-   ECG_State  : Sensor_State; 
-   Param : PanTompkins.Config := (others => <>);
+   Raw_Command :UART_USB.UART_String;
+   Arg : Commands_Interpreter.Argument;
 
 begin
-
+   
    begin
-      UART_USB.Initialize (115_200);
-      UART_USB.Transmit_String ("ECG_SENSOR v0.1");
-
-      Set_Default_Parameters;
+      Initialize;
 
       loop
-
-         case ECG_State is
+         case Sensor_State.Get_Value is
             when INIT =>
-               Config := Read_Param;
-               PanTompkins.Initialize (Config);
+               
+               loop
+                  Raw_Command := UART_USB.Receive_String (ASCII.Semicolon, Time_Span_Last);
+                  
+                  begin
+                     Arg := Commands_Interpreter.Parse (UART_USB.B_Str.To_String (Raw_Command));
+                  exception
+                     when C : Commands_Interpreter.Commands_Exception =>
+                        Log (Exception_Message (C));
+                  end;
+                  
+                  exit when Sensor_State.Get_Value = SAMPLING;
+               end loop;
+               
+               PanTompkins.Initialize ((Sampling_Frequency => Sample_Rate.Get_Value,
+                                        Amplitude_Treshold_Coef => Amplitude_Coef.Get_Value,
+                                        Minimal_Pick_Distance_Sec => Pick_Distance.Get_Value,
+                                        Window_Sec => Window_Sec.Get_Value,
+                                        Output_Stage => Output_Stage.Get_Value));
             when SAMPLING =>
                for I in AdaData.Data'Range loop
                   -- Read commands
                   Result := PanTompkins.Process_Sample (AdaData.Data(I));
-                  UART_USB.Transmit_String(Int16(IEEE_Float_32'Max(IEEE_Float_32'Min(Result * 1000.0, 2.0**15 - 1.0), -2.0**15))'Image);
+                  UART_USB.Transmit_String(UART_USB.Int16(IEEE_Float_32'Max(IEEE_Float_32'Min(Result * 1000.0, 2.0**15 - 1.0), -2.0**15))'Image);
                end loop;
             when others =>
 
@@ -146,9 +148,8 @@ begin
 
    exception -- Unknows Errors (if UART is working..)
       when C : Constraint_Error =>
-         UART_USB.Transmit_String (Exception_Message (C));
+         Log (Exception_Message (C));
       when P : Program_Error => 
-         UART_USB.Transmit_String (Exception_Message (P));
-      end;
-
+         Log (Exception_Message (P));
+   end;
 end Ecg_Sensor;
